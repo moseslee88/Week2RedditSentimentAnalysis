ddf367cbfbd17f632ce98fde475f504ba0b624c9
Moses Lee <moseslee88@gmail.com>
2020-09-01 20:52:33 -0600

    Updated credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,5 @@
-# note: these credentials are not valid, replace with your own
+# note: these credentials are not valid, replace with your own  
+##EDIT: replaced with my own personal credentials
 client_id = '5IhZscpEj4ULow'
 client_secret = 'hauiAFMbEQgoAQA0N8IioYNB8ZU'
 user_agent = 'MSDS600 Scrape'
