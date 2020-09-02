9fd52528ff355d8a67015a7cc670d5b71e91b3bd
Moses Lee <moseslee88@gmail.com>
2020-09-01 22:12:47 -0600

    Worked to plot Sentiment value

--- Reddit_Sentiment_Assignment.py
+++ Reddit_Sentiment_Assignment.py
@@ -18,14 +18,16 @@ time_period = 'all'  # use posts from all time
 # .top() can use the time_period argument
 # subreddit = reddit.subreddit('').top(time_filter=time_period, limit=number_of_posts)
 
-subreddit = reddit.subreddit('nba').hot(limit=number_of_posts)
+###subreddit = reddit.subreddit('nba').hot(limit=number_of_posts)
+subreddit = reddit.subreddit('nba').hot(limit=400)
 
 # Create an empty list to store the data
 subreddit_comments = []
 
 # go through each post in our subreddit and put the comment body and id in our dictionary
 # the value for 'total' here needs to match 'limit' in reddit.subreddit().top()
-for post in tqdm(subreddit, total=number_of_posts):
+for post in tqdm(subreddit, total=400):
+###for post in tqdm(subreddit, total=number_of_posts):
     submission = reddit.submission(id=post)
     submission.comments.replace_more(limit=0)  # This line of code expands the comments if “load more comments” and “continue this thread” links are encountered
     for top_level_comment in submission.comments:
@@ -35,6 +37,11 @@ print("test TEST")
 ##subreddit_comments
 print(subreddit_comments)
 print("...end TESTing")
+
+# Store comments in a DataFrame using a dictionary as our input
+# This sets the column name as the key of the dictionary, and the list of values as the values in the DataFrame
+subreddit_comments_df = pd.DataFrame(data={'comment': subreddit_comments})
+subreddit_comments_df
 
 # This is an example of how we split up the comments into individual words.
 # This technique will be used again to get the scores of each individual word.
@@ -49,7 +56,10 @@ for comment in subreddit_comments_df['comment']:  # loop over each word
 
 # We load the AFINN sentiment table into a Python dictionary
 
-sentimentfile = open("AFINN-en-165.txt", "r")  # open sentiment file
+sentimentfile = open("AFINN-en-165.txt", "r")  # open sentiment file    \
+####sentimentfile = open("/Users/moseslee88/Week2RedditSentimentAnalysis/AFINN-en-165.txt", "r")  # open sentiment file
+###/Users/moseslee88/Documents/GitHub/Week2RedditSentimentAnalysis/AFINN-en-165.txt
+####/Users/moseslee88/Documents/GitHub/Week2RedditSentimentAnalysis/AFINN-en-165.txt
 scores = {}  # an empty dictionary
 for line in sentimentfile:  # loop over each word / sentiment score
     word, score = line.split("\t")  # file is tab-delimited
@@ -67,24 +77,106 @@ for key, value in scores.items():
 
 ####
 # we create a dictionary for storing overall counts of sentiment values
-##sentiments = {"-5": 0, "-4": 0, "-3": 0, "-2": 0, "-1": 0, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
+sentiments = {"-5": 0, "-4": 0, "-3": 0, "-2": 0, "-1": 0, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
 
-##for word in subreddit_comments_df['comment']:  # loop over each word
-##        comment_words = word.split()  # split comments into individual words
-##        for word in comment_words:  # loop over individual words in each comment
-##            word = word.strip('?:!.,;"!@()#-')  # remove extraneous characters
-##            word = word.replace("\n", "")  # remove end of line
-##            if word in scores.keys():  # check if word is in sentiment dictionary
-##                score = scores[word]  # check if word is in sentiment dictionary
-##                sentiments[str(score)] += 1  # add one to the sentiment score
+for word in subreddit_comments_df['comment']:  # loop over each word
+        comment_words = word.split()  # split comments into individual words
+        for word in comment_words:  # loop over individual words in each comment
+            word = word.strip('?:!.,;"!@()#-')  # remove extraneous characters
+            word = word.replace("\n", "")  # remove end of line
+            if word in scores.keys():  # check if word is in sentiment dictionary
+                score = scores[word]  # check if word is in sentiment dictionary
+                sentiments[str(score)] += 1  # add one to the sentiment score
 
 # Print the scores
-#### for sentiment_value in range(-5, 6):
+for sentiment_value in range(-5, 6):
     # this uses string formatting, more on this here: https://realpython.com/python-f-strings/
-    ####### print(f"{sentiment_value} sentiment:", sentiments[str(sentiment_value)])
+    print(f"{sentiment_value} sentiment:", sentiments[str(sentiment_value)])
 
 # this would be equivalent, but obviously much less compact and elegant
 # print("-5 sentiments ", sentiments["-5"])
 # print("-4 sentiments ", sentiments["-4"])
 # print("-3 sentiments ", sentiments["-3"])
 # print("-2 sentiments ", sentiments["-2"])
+
+#########################
+# Now let us put the sentiment scores into a dataframe.
+comment_sentiment_df = pd.DataFrame(data={'Sentiment_Value': list(sentiments.keys()), 'Counts': list(sentiments.values())})
+# the 'value' column is a string; convert to integer (numeric type)
+comment_sentiment_df['Sentiment_Value'] = comment_sentiment_df['Sentiment_Value'].astype('int')
+
+# We normalize the counts so we will be able to compare between two subreddits on the same plot easily
+comment_sentiment_df['Normalized_Counts'] = comment_sentiment_df['Counts'] / comment_sentiment_df['Counts'].sum()  # Normalize the Count
+comment_sentiment_df
+
+##########
+# Count vs Sentiment Value Plot
+plt.bar(comment_sentiment_df['Sentiment_Value'], comment_sentiment_df['Count'], color='blue')  # add the y-values and color
+plt.xlabel('sentiment value')  # add x-label
+plt.ylabel('count')  # add y-label
+plt.title('Count Vs Sentiments')  # add title
+plt.show()
+  
+# Normalized Counts vs Sentiment Value Plot
+plt.bar(comment_sentiment_df['Normalized_Counts'], comment_sentiment_df['Count'], color='grey')  # add the y-values and color
+###plt.bar(comment_sentiment_df['Sentiment_Value'], comment_sentiment_df['Count'], color='grey')  # add the y-values and color
+plt.xlabel('sentiment value')  # add x-label
+plt.ylabel('count')  # add y-label
+plt.title('Normalized Cnts vs Sentiment')  # add title
+plt.show()
+
+
+
+subreddit_2 = reddit.subreddit('baseball').hot(limit=number_of_posts)
+
+
+# Create an empty list to store the data
+subreddit_comments_2 = []
+
+# go through each post in our subreddit and put the comment body and id in our dictionary
+for post in tqdm(subreddit_2, total=number_of_posts):
+    submission = reddit.submission(id=post)
+    submission.comments.replace_more(limit=0)  # This line of code expands the comments if “load more comments” and “continue this thread” links are encountered
+    for top_level_comment in submission.comments:
+        subreddit_comments_2.append(top_level_comment.body)  # add the comment to our list of comments
+
+
+# Store comments in a DataFrame using a dictionary as our input
+# This sets the column name as the key of the dictionary, and the list of values as the values in the DataFrame
+subreddit_comments_df_2 = pd.DataFrame(data={'comment': subreddit_comments_2})
+
+# we create a dictionary for storing overall counts of sentiment values
+sentiments_2 = {"-5": 0, "-4": 0, "-3": 0, "-2": 0, "-1": 0, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0}
+
+for comment in subreddit_comments_df_2['comment']:  # loop over each comment
+        comment_words = comment.split()  # split comments into individual words
+        for word in comment_words:  # loop over individual words in each comment
+            word = word.strip('?:!.,;"!@()#-')  # remove extraneous characters
+            word = word.replace("\n", "")  # remove end of line
+            if word in scores.keys():  # check if word is in sentiment dictionary
+                score = scores[word]  # check if word is in sentiment dictionary
+                sentiments_2[str(score)] += 1  # add one to the sentiment score
+
+# Now let us put the sentiment scores into a dataframe.
+comment_sentiment_df_2 = pd.DataFrame(data={'Sentiment_Value': list(sentiments_2.keys()), 'Counts': list(sentiments_2.values())})
+# the 'value' column is a string; convert to integer (numeric type)
+comment_sentiment_df_2['Sentiment_Value'] = comment_sentiment_df_2['Sentiment_Value'].astype('int')
+
+# We normalize the counts so we will be able to compare between two subreddits on the same plot easily
+comment_sentiment_df_2['Normalized_Counts'] = comment_sentiment_df_2['Counts'] / comment_sentiment_df_2['Counts'].sum()  # Normalize the Count
+comment_sentiment_df_2
+
+
+# Count vs Sentiment Value Plot
+###plt.bar(comment_sentiment_df_2['Sentiment_Value'], comment_sentiment_df_2[''], color='')  # add the y-values and color
+###plt.xlabel('')  # add x-label
+###plt.ylabel('')  # add y-label
+###plt.title('')  # add title
+####plt.show()
+
+# Normalized Counts vs Sentiment Value Plot
+####plt.bar(comment_sentiment_df_2['Sentiment_Value'], comment_sentiment_df_2[''], color='')  # add the y-values and color
+####plt.xlabel('')  # add x-label
+###plt.ylabel('')  # add y-label
+####plt.title('')  # add title
+#####plt.show()
